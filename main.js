bb1caffdaeada4313e120601ef38d47bcf70d7c6
Ekizochikku <bri565@uw.edu>
2018-01-23 13:08:47 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,95 @@
+	var AM = new AssetManager();
+
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
+	this.spriteSheet = spriteSheet;
+	this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.scale = scale;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
+
+    ctx.drawImage(this.spriteSheet,
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth * this.scale,
+                 this.frameHeight * this.scale);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+function Background(game, spritesheet) {
+    this.x = 0;
+    this.y = 0;
+    this.spritesheet = spritesheet;
+    this.game = game;
+    this.ctx = game.ctx;
+};
+
+Background.prototype.draw = function () {
+    this.ctx.drawImage(this.spritesheet,
+                   this.x, this.y);
+};
+
+Background.prototype.update = function () {
+};
+
+function Gloria(game, spriteSheet) {
+	this.animation = new Animation(spriteSheet, 112, 900, 9, 0.2, 9, true, 0.5);
+	this.speed = 150;
+	this.ctx = game.ctx;
+	Entity.call(this, game, 0, 100);
+}
+
+Gloria.prototype = new Entity();
+Gloria.prototype.constructor = Gloria;
+
+Gloria.prototype.update = function () {
+    this.x += this.game.clockTick * this.speed;
+    if (this.x > 800) this.x = -100;
+    Entity.prototype.update.call(this);
+}
+
+Gloria.prototype.draw = function () {
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
+}
+
+
+AM.queueDownload("./img/gloriasprites.png");
+AM.queueDownload("./img/background.jpg");
+
+AM.downloadAll(function () {
+	var canvas = document.getElementById("gameWorld");
+	var ctx = canvas.getContext("2d");
+	var gameEngine = new GameEngine();
+	gameEngine.init(ctx);
+	gameEngine.start();
+	
+	gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background.jpg")));
+	gameEngine.addEntity(new Gloria(gameEngine, AM.getAsset("./img/gloriasprites.png")));
+	
+	console.log("Done");
+});
